54814359d9259cfb0c839aabee9156312c67b32a
blutterfly <137254356+blutterfly@users.noreply.github.com>
2026-08-23 12:56:49 -0400

    Deployed fde22a4 with MkDocs version: 1.6.1

---
+++ assets/javascripts/extra.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-current-year]").forEach((element) => {
+    element.textContent = new Date().getFullYear();
+  });
+});
